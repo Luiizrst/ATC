8563f2ade069308b0cf841f7b86de5be4ae9fba7
Luiz Guilherme <37851595+Luiizrst@users.noreply.github.com>
2022-07-27 11:57:08 -0300

    Add files via upload

---
+++ TrabalhoDCC146 FINAL/src/trabalhodcc146/TrabalhoDCC146.java
@@ -0,0 +1,30 @@
+package trabalhodcc146;
+
+
+import Componentes.Menu;
+
+/**
+ * TRABALHO PRÁTICO - DCC146 ASPECTOS TEÓRICOS DA COMPUTAÇÃO 2021/1
+ * Professor: GLEIPH GHIOTTO LIMA DE MENEZES
+ * Autores: Débora Izabel Rocha Duarte     Matrícula: 201776029
+ *          Fabrício de Sousa Guidine      Matrícula: 
+ *          Juarez de Paula Campos Júnior  Matrícula: 201676022
+ *          Luiz Guilherme Almas Araujo    Matrícula: 201676050
+ */
+
+
+/**
+ *
+ * @author izabe
+ */
+public class TrabalhoDCC146 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Menu m = new Menu();
+        m.menu();
+    }
+    
+}
